7125eaa62b392fe88442f415c02323ea28cd4cb5
Oladiti530 <oladitilukuman09@gmail.com>
2025-08-15 06:19:38 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+// for img changing
+
+const defImg = document.getElementById('deImg');
+
+defImg.addEventListener('click', function(){
+    document.getElementById('bg').style.backgroundImage = "url(IMG_4834.JPG)";
+});
+
+
+function firstImg(){
+    document.getElementById('bg').style.backgroundImage = "url('IMG_4835.JPG')";
+}
+
+function secondImg(){
+    document.getElementById('bg').style.backgroundImage = "url('IMG_4848.JPG')";
+}
+
+// for menu 
+
+function opn(){    
+
+}
+function cls(){    
+
+}
